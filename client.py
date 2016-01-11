0fda4b815005158c2badf5bb371bef23b8c489c0
Erik Thorselius <erik.thorselius@gmail.com>
2016-01-11 23:00:27 +0100

    Wrong heartbeat id

--- client.py
+++ client.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     scheduler.start()
     while True:
         try:
-            socket.send_multipart([b'health_check', bytes(json.dumps({'type': 'health_check', 'id': 'proxy'}), 'utf8')])
+            socket.send_multipart([b'health_check', bytes(json.dumps({'type': 'health_check', 'id': 'sunrise-sunset-reporter'}), 'utf8')])
             sleep(10)
         except (KeyboardInterrupt, SystemExit):
             print('Exiting!')
